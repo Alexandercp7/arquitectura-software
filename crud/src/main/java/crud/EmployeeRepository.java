53d65f20a9d156c59861cdf9a2664787c8c0e9be
Alexandercp7 <A22216350@alumnos.uady.com>
2024-12-06 05:35:17 -0600

    Me equivoque xd

--- crud/src/main/java/crud/EmployeeRepository.java
+++ crud/src/main/java/crud/EmployeeRepository.java
@@ -4,8 +4,8 @@ import crud.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-import java.util.Optional;
 
+import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findEmployeeByName(String name);
 }
